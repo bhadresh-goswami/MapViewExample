369707ac6331c2c3d8d555b014aefa4ad651ab3f
Sanket Chauhan <sanketchauhan.cmpica@gmail.com>
2019-02-22 12:53:19 +0530

    updates

--- MapViewExample/ViewController.swift
+++ MapViewExample/ViewController.swift
@@ -7,11 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+import CoreLocation
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var annotations = [MKPointAnnotation]()
+    
+    var clManager:CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.sizeToFit()
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 23.0225, longitude: 72.5714)
+        
+        annotations.append(annotation)
+        mapView.addAnnotation(annotation)
+
+        
+        
+        
+//        var zoomRect = MKMapRectNull;
+//        let myLocationPointRect = MKMapRectMake(23.0225, 72.5714, 0, 0)
+//        //var currentDestinationPointRect = MKMapRectMake(currentDestination.longitude, currentDestination.latitude, 0, 0)
+//
+//        //zoomRect = myLocationPointRect;
+//        zoomRect = MKMapRectUnion(zoomRect, myLocationPointRect);
+//        mapView.setVisibleMapRect(zoomRect, animated: true)
+
+        
+        
+        
+        clManager = CLLocationManager()
+        clManager?.delegate = self
+        clManager?.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +58,29 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("i am at new location!")
+        let newLocation = locations.last
+        
+        
+//        let coordinates = CLLocationCoordinate2DMake((newLocation?.coordinate.latitude)!, newLocation?.coordinate.longitude)
+        
+        //mapView.removeAnnotations(annotations)
+        
+        let annotation = MKPointAnnotation()
+        annotations.append(annotation)
+
+        annotation.coordinate = CLLocationCoordinate2D(latitude: (newLocation?.coordinate.latitude)!, longitude: (newLocation?.coordinate.longitude)!)
+        mapView.addAnnotation(annotation)
+        let regionRadius: CLLocationDistance = 50000
+
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance((newLocation?.coordinate)!,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+        
+    }
+    
+}
